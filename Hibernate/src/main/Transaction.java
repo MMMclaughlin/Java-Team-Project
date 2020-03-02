ac64031902e515bfb0101f334e288455e08ad9ed
b9012721 <b9012721@newcastle.ac.uk>
2020-03-02 15:10:00 +0000

    Updateed transaction class

--- Hibernate/src/main/Transaction.java
+++ Hibernate/src/main/Transaction.java
@@ -1,13 +1,20 @@
 package main;
 
-public class Transactions {
+public class Transaction {
+
     //establish database connection here
     public static void main(String[] args){
 
     }
-    public static void findEntity(String itemName){
+    public static void findItem(Item item){
         //find item by name or id? might want to ask specification requirement here
 
+
+    }
+    public static void updateEntity(Item item){
+
+    }
+    public static void deleteItem(Item item){
 
     }
 }

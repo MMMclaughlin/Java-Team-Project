12d5e8437b27a53736f17fe5f3ad0be9d2b44a48
b9015456 <b9015456@newcastle.ac.uk>
2020-03-02 22:56:03 +0000

    Added UpdateStock method in Transaction

--- src/main/Transaction.java
+++ src/main/Transaction.java
@@ -3,11 +3,12 @@ package main;
 import org.hibernate.*;
 import javax.persistence.Entity;
 public class Transaction {
-
     //establish database connection here
+
     public static void main(String[] args){
 
     }
+
     public static Item findItem(int id){
         Session session = HibernateUtil.getSessionFactory().openSession();//creates a session
         session.beginTransaction();// begin transaction
@@ -15,8 +16,21 @@ public class Transaction {
         session.close();//closes current session
         return item;//return found item or null if item wasnt found
     }
-    public static void updateEntity(Item item){
 
+    public static void updateStock(int id, int newStock){
+        try
+        {
+            Session session = HibernateUtil.getSessionFactory().openSession(); // Create a session
+            session.beginTransaction(); // Begin the transaction
+            Item item = session.get(Item.class, id); // Gets the item from the database
+            item.setStock(newStock); // Sets the item stock level to the new stock
+            session.update(item); // Updates the item's stock in the database
+            session.close();
+        }
+        catch(HibernateException e)
+        {
+            e.printStackTrace();
+        }
     }
     public static void deleteItem(Item item){
 

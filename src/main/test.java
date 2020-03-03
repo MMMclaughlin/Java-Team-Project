0581e7eba9276a8c0b9570ef819d713afb6cb1a8
b9012721 <b9012721@newcastle.ac.uk>
2020-03-03 14:21:45 +0000

    Edited test to run full menu

--- src/main/test.java
+++ src/main/test.java
@@ -5,7 +5,7 @@ public class test{
 
     // this is a file for running a specific data set or queries and can be edited freely.
     public static void main(String[] args){
-        Sale sale = new Sale();
-        sale.sale();
+        epos epos = new epos();
+        epos.choice();
     }
 }
